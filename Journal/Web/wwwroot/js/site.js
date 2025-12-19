6b2b4a9c4277c2b2c714dcd9d0a0080602d206be
Alex Sanderson <67873187+Vexelior@users.noreply.github.com>
2025-12-19 07:26:27 -0800

    Add nested dropdowns for journals and prompts in navbar

--- Journal/Web/wwwroot/js/site.js
+++ Journal/Web/wwwroot/js/site.js
@@ -1,4 +1,43 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿document.addEventListener('DOMContentLoaded', function () {
+    document.querySelectorAll('.dropdown-submenu > .dropdown-toggle').forEach(function (element) {
+        element.addEventListener('click', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            var submenu = this.nextElementSibling;
+            if (submenu) {
+                submenu.classList.toggle('show');
+            }
+        });
+    });
+    if (window.matchMedia('(min-width: 768px)').matches) {
+        document.querySelectorAll('.dropdown-submenu').forEach(function (element) {
+            element.addEventListener('mouseenter', function () {
+                var submenu = this.querySelector('.dropdown-menu');
+                if (submenu) {
+                    submenu.classList.add('show');
+                }
+            });
+            element.addEventListener('mouseleave', function () {
+                var submenu = this.querySelector('.dropdown-menu');
+                if (submenu) {
+                    submenu.classList.remove('show');
+                }
+            });
+        });
+        document.querySelectorAll('.dropdown').forEach(function (element) {
+            element.addEventListener('mouseenter', function () {
+                var menu = this.querySelector('.dropdown-menu');
+                if (menu) {
+                    menu.classList.add('show');
+                }
+            });
 
-// Write your JavaScript code.
+            element.addEventListener('mouseleave', function () {
+                var menu = this.querySelector('.dropdown-menu');
+                if (menu) {
+                    menu.classList.remove('show');
+                }
+            });
+        });
+    }
+});
