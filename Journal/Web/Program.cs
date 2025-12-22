2b6328f5cb5798543322bdc6d8cb04789e340630
Alex Sanderson <67873187+Vexelior@users.noreply.github.com>
2025-12-22 04:52:56 -0800

    Integrate Summernote editor and update configs

--- Journal/Web/Program.cs
+++ Journal/Web/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Infrastructure.Repository;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddJsonFile($"appsettings.{builder.Environment.EnvironmentName}.json", optional: true, reloadOnChange: true);
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
